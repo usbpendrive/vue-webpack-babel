c6b166f8d5d7d768c8a498407da41c33f0f1b16b
usbpendrive <ferry.fernando@gmail.com>
2018-12-09 08:23:32 +0700

    Babel & webpack settings.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+};
